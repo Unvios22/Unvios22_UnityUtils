8633305bc5b42a73b62ccdb26e3f512e461ca637
Kajetan Jóźwiak <unvios22@gmail.com>
2021-07-13 03:48:34 +0200

    Add internal consumer Action<> decoupling to CoroutineUtils

--- Assets/Unvios22 UnityUtils/Runtime/Coroutines/CoroutineUtils.cs
+++ Assets/Unvios22 UnityUtils/Runtime/Coroutines/CoroutineUtils.cs
@@ -7,30 +7,49 @@ namespace Unvios22_UnityUtils.Runtime.Coroutines {
 	public static class CoroutineUtils {
 		
 		/// <summary>
-		/// Creates a coroutine on <paramref name="coroutineTarget"/> object, and lerps between <paramref name="fromValue"/>
-		/// and <paramref name="toValue"/> over the course of <paramref name="lerpTime"/> seconds.
-		/// Invokes the <paramref name="valueSetterAction"/> Action every frame with current lerp value.
+		/// Starts a coroutine on <paramref name="coroutineTarget"/> object that Lerps between <paramref name="from"/>
+		/// and <paramref name="to"/> over the course of <paramref name="lerpTime"/> seconds.
+		/// Invokes the <paramref name="consumer"/> Action every frame with current Lerp value.
 		/// </summary>
-		/// <returns>Reference to the created coroutine</returns>
-		public static Coroutine LerpFloat(MonoBehaviour coroutineTarget, float fromValue, float toValue, float lerpTime,
-			[NotNull]Action<float> valueSetterAction) {
-			//I tried to use ref or out args, but neither anonymous functions nor enumerating functions (i.e. coroutines) can use them
-			//https://docs.microsoft.com/pl-pl/archive/blogs/ericlippert/iterator-blocks-part-two-why-no-ref-or-out-parameters
-			if (valueSetterAction == null) {
-				throw new ArgumentNullException();
-			}
-			return coroutineTarget.StartCoroutine(InternalLerpCoroutine(fromValue, toValue, lerpTime, valueSetterAction));
+		/// <returns>Reference to the started coroutine</returns>
+		public static Coroutine LerpFloat(MonoBehaviour coroutineTarget, float from, float to,
+			[NotNull]Action<float> consumer, float lerpTime) {
+			/*I tried to use ref or out args, but neither anonymous nor enumerating functions (i.e. coroutines) can use them:
+			 https://docs.microsoft.com/pl-pl/archive/blogs/ericlippert/iterator-blocks-part-two-why-no-ref-or-out-parameters */
+			ThrowArgumentNullExceptionIfConsumerNull(consumer);
+			
+			Action<float> lerpFunction = (f) => { consumer.Invoke(Mathf.Lerp(from, to, f));};
+			return coroutineTarget.StartCoroutine(InterpolateWithConsumerActionCoroutine(lerpFunction, lerpTime));
+		}
+		
+		/// <summary>
+		/// Starts a coroutine on <paramref name="coroutineTarget"/> object that Slerps between <paramref name="from"/>
+		/// and <paramref name="to"/> over the course of <paramref name="lerpTime"/> seconds.
+		/// Invokes the <paramref name="consumer"/> Action every frame with current Slerp value.
+		/// </summary>
+		/// <returns>Reference to the started coroutine</returns>
+		public static Coroutine SlerpFloat(MonoBehaviour coroutineTarget, float from, float to,
+			[NotNull]Action<float> consumer, float slerpTime) {
+			ThrowArgumentNullExceptionIfConsumerNull(consumer);
+
+			Action<float> slerpFunction = (f) => { consumer.Invoke(Mathf.SmoothStep(from, to, f));};
+			return coroutineTarget.StartCoroutine(InterpolateWithConsumerActionCoroutine(slerpFunction, slerpTime));
 		}
 
-		private static IEnumerator InternalLerpCoroutine(float fromValue, float toValue, float lerpTime,
-			Action<float> updateValueAction) {
+		private static void ThrowArgumentNullExceptionIfConsumerNull<T>(Action<T> consumer) {
+			if (consumer == null) {
+				throw new ArgumentNullException(nameof(consumer), "Consumer cannot be null");
+			}
+		}
+		
+		private static IEnumerator InterpolateWithConsumerActionCoroutine(Action<float> consumer, float interpolationTime) {
 			var timer = 0f;
-			var lerpPercentage = 0f;
-			while (timer < lerpTime) {
+			
+			while (timer < interpolationTime) {
 				yield return null;
 				timer += Time.deltaTime;
-				lerpPercentage = timer / lerpTime;
-				updateValueAction(Mathf.Lerp(fromValue, toValue, lerpPercentage));
+				var interpolationPercentage = timer / interpolationTime;
+				consumer.Invoke(interpolationPercentage);
 			}
 		}
 	}
