a7d6fcbabce3c622a65400be098c9a5059df9431
Kajetan Jóźwiak <unvios22@gmail.com>
2021-07-15 16:08:07 +0200

    Add FileSystem utils

---
+++ Assets/Unvios22 UnityUtils/Editor/FileSystem/FileFinder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Unvios22_UnityUtils.Editor.FileSystem {
+	public class FileFinder {
+		public static T FindUniqueAssetByFileName<T>(string assetFileName) where T : class {
+			
+			//using System.IO instead of AssetDatabase because the database won't find files with exact extension given.
+			var foundFiles = Directory.GetFiles(Application.dataPath + @"\", $"{assetFileName}",
+				SearchOption.AllDirectories);
+			
+			if (foundFiles.Length != 1) {
+				throw new ArgumentException($"Found:{foundFiles.Length} '{assetFileName}' files!");
+			}
+			var foundAssetPath = foundFiles[0];
+			var foundAssetPathRelativeToProjectFolder = foundAssetPath.Replace(Application.dataPath,
+				@"Assets\");
+			var foundAsset = AssetDatabase.LoadAssetAtPath(foundAssetPathRelativeToProjectFolder, typeof(T)) as T;
+			return foundAsset;
+		}
+	}
+}
